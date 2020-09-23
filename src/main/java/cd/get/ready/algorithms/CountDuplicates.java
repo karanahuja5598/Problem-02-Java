25f64b77e570fe0a518ae4595ab78638104c2045
Karan Ahuja <karanahuja5598@gmail.com>
2020-09-23 10:14:04 -0500

    Finished everything

--- src/main/java/cd/get/ready/algorithms/CountDuplicates.java
+++ src/main/java/cd/get/ready/algorithms/CountDuplicates.java
@@ -1,5 +1,9 @@
 package cd.get.ready.algorithms;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
@@ -7,7 +11,25 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(String[] input){
-        return null;
+        Map<String,Integer> repeatedWords = new HashMap<String,Integer>();
+        for(String str : input){
+
+            if(repeatedWords.containsKey(str)) {
+                repeatedWords.put(str,repeatedWords.get(str) + 1);
+            }
+            else {
+                repeatedWords.put(str, 1);
+            }
+        }
+
+        int count = 0;
+        for(int x : repeatedWords.values()){
+            if(x > 1) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     /**
@@ -16,6 +38,25 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(Integer[] input){
-        return null;
+        Arrays.sort(input);
+
+        int numberDuplicates = 0;
+        int duplicateCount = 0;
+        int previousVal = -1;
+
+        for (int i=0; i < input.length; ++i) {
+            if (input[i] == previousVal) {
+                ++numberDuplicates;
+                if (numberDuplicates == 1) {
+                    ++duplicateCount;
+                }
+            }
+            else {
+                previousVal = input[i];
+                numberDuplicates = 0;
+            }
+        }
+
+        return duplicateCount;
     }
 }
